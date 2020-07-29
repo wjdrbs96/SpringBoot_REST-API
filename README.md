3f3f69ea424f45b0400549d625464959c29647e6
최정균 <wjdrbs966@naver.com>
2020-07-29 19:43:30 +0900

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # SpringBoot REST API 
+
+### To Do
+
+* Web Service & Web Application
+
+* Spring Boot & RESTful Service
+
+* User Service API 구현
+
+* RESTful Service 기능 확장
+ 
+* Spring Boot API 사용
+
+* Java Persistence API 사용
+
+* RESTful API 설계 가이드 
--- src/main/java/com/example/restfulwebservice/helloworld/HelloWorldBean.java
+++ src/main/java/com/example/restfulwebservice/helloworld/HelloWorldBean.java
@@ -3,11 +3,16 @@ package com.example.restfulwebservice.helloworld;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RestController;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class HelloWorldBean {
     private String message;
-
 }
