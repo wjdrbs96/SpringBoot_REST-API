7d6a0b690b94a8fe855dbaea43bed63e9b44e994
최정균 <wjdrbs966@naver.com>
2020-07-29 23:57:08 +0900

    spring boot Swagger Domain Update

--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -1,6 +1,8 @@
 package com.example.restfulwebservice.user;
 
 import com.fasterxml.jackson.annotation.JsonFilter;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,16 +14,22 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
 //@JsonFilter("UserInfo")
 public class User {
     private Integer id;
 
     @Size(min = 2, message = "Name is two over")
+    @ApiModelProperty(notes = "사용자 이름을 입력해주세요")
     private String name;
 
     @Past
+    @ApiModelProperty(notes = "사용자의 등록일을 입력해 주세요")
     private Date joinDate;
 
+    @ApiModelProperty(notes = "사용자의 비밀번호를 입력해 주세요")
     private String password;
+
+    @ApiModelProperty(notes = "사용자의 주민번호를 입력해 주세요")
     private String ssn;
 }
