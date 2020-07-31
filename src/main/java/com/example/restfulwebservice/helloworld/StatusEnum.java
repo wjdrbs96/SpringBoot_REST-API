c0c6d91634d95056d6766891df1ed57b6d368b9c
최정균 <wjdrbs966@naver.com>
2020-07-31 22:09:39 +0900

    statusCode add

--- src/main/java/com/example/restfulwebservice/helloworld/StatusEnum.java
+++ src/main/java/com/example/restfulwebservice/helloworld/StatusEnum.java
@@ -5,7 +5,8 @@ public enum StatusEnum {
     OK(200, "OK"),
     BAD_REQUEST(400, "BAD_REQUEST"),
     NOT_FOUND(404, "NOT_FOUND"),
-    INTERNAL_SERER_ERROR(500, "INTERNAL_SERVER_ERROR");
+    INTERNAL_SERER_ERROR(500, "INTERNAL_SERVER_ERROR"),
+    UNAUTHORIZED(401, "UNAUTHORIZED");
 
     int statusCode;
     String code;
